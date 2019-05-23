b63fa49d1a84f91b68978db6fb3a373aaa015aa3
Jakedalus <jacquedalus@gmail.com>
2019-05-23 15:13:49 -0400

    Add create todo post route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser');
 
 var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
  res.json({message: 'HI FROM ROOT ROUTE!'});
