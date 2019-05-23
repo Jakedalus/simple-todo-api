4eaee4f7a2013d0d1505ac8829227706443ea20e
Jakedalus <jacquedalus@gmail.com>
2019-05-23 14:34:54 -0400

    Add routes dir and todos get route

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var todoRoutes = require('./routes/todos');
 
 app.get('/', function(req, res) {
- res.json({message: 'HI FROM EXPRESS!'});
+ res.json({message: 'HI FROM ROOT ROUTE!'});
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function() {
   console.log("App is running on port ", port);
