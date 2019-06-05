94659499a7115119a134a24ed46a3efec0018fd4
Jakedalus <jacquedalus@gmail.com>
2019-06-05 16:17:21 -0400

    Implement creating and adding todos

--- public/app.js
+++ public/app.js
@@ -2,17 +2,41 @@ $(document).ready(function() {
   $.getJSON('/api/todos')
   .done(addTodos)
   .fail(handleError);
+  
+  $('#todoInput').keypress(function(event) {
+    if (event.which === 13) {
+      createTodo();
+    }
+  });
 });
 
 
 function addTodos(todos) {
   todos.forEach(function(todo) {
-    var newTodo = $('<li class="task">' + todo.name + '</li>');
-    $('.list').append(newTodo);
+    addTodo(todo);
   });
 }
 
 
 function handleError(err) {
   console.log(err);
+}
+
+function createTodo() {
+  var userInput = $('#todoInput').val();
+  $.post('/api/todos', {name: userInput})
+  .done(function(newTodo) {
+    addTodo(newTodo);
+    $('#todoInput').val('');
+  })
+  .fail(handleError);  
+  
+}
+
+function addTodo(todo) {
+  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
 }
